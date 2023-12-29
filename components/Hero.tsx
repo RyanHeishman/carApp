97fd3e6d490e1094e07ec677832a19f91bfae916
RyanHeishman <heishy0@gmail.com>
2023-12-28 18:31:16 -0800

    carr app

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Image from "next/image"
 import { CustomButton } from "."
 
 const Hero = () => {
+
+  const handleScroll = () => {
+
+  }
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -14,11 +21,18 @@ const Hero = () => {
           Streamline your car rental experience with our effortless booking process.
         </p>
 
-        <CustomButton />
+        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}/>
 
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" fill className="object-contain"  alt="" />
+        </div>
+          <div className="hero__image-overlay"/>
       </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero 
