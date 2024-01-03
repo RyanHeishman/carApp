dd99cb93cd0f2f0b01734212ddd7a8cbeba14733
RyanHeishman <heishy0@gmail.com>
2024-01-03 12:53:36 -0800

    car app complete

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,7 +6,7 @@ import { Combobox, Transition } from '@headlessui/react'
 import { useState, Fragment } from 'react';
 import { manufacturers } from '@/constants/indes';
 
-const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+const SearchManufacturer = ({selected, setSelected}: SearchManufacturerProps) => {
     
     const [query, setQuery] = useState('');
 
@@ -21,7 +21,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
 
   return (
     <div className='search-manufacturer'>
-        <Combobox value={manufacturer} onChange={setManufacturer}>
+        <Combobox value={selected} onChange={setSelected}>
             <div className='relative w-full'>
                 <Combobox.Button className="absolute top-[14px]">
                     <img src="/car-logo.svg" width={20} height={20} className='ml-4' alt="" />
